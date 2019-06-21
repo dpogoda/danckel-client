e10b80f47ac5d8890b31c522b11fc3faa12767fc
David Pogorzelski <d.pogorzelski91@gmail.com>
2019-06-21 22:06:47 +0200

    Add error messages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import _ from 'lodash';
 import { navigate } from 'gatsby';
 import gql from 'graphql-tag';
 import { compose, graphql } from 'react-apollo';
@@ -9,22 +10,33 @@ import Button from '../components/cowo/mobile/button/Button';
 const SignIn = ({ signIn }) => {
   const [roomnumber, setRoomnumber] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   /* eslint-disable */
   if (typeof window !== 'undefined' && window.localStorage.getItem('token')) {
     navigate('./request/');
+  } else if (loading) {
+    setLoading(false);
   }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   /* eslint-disable */
 
   return (
     <div>
       <div>
         <h3 style={{ paddingLeft: '5vw' }}>Login</h3>
+        <p style={{ paddingLeft: '5vw' }}>{error}</p>
         <div style={{ textAlign: 'center' }}>
           <TextField
             label="Room number"
             placeholder="e.g. 112"
             onChange={value => setRoomnumber(value)}
+            type="number"
           />
           <TextField
             type="password"
@@ -38,30 +50,40 @@ const SignIn = ({ signIn }) => {
       <ButtonContainer type="loose">
         <Button
           text="Sign In"
+          loading={loading}
           onPress={() => {
             if (roomnumber !== '' && password !== '') {
+              setLoading(true);
               signIn({
                 variables: {
                   roomnumber: parseInt(roomnumber, 10),
                   password,
                 },
               })
-                .catch(error => {
-                  console.log(error);
-                })
                 .then(signInResult => {
+                  setError('');
                   const {
                     data: {
-                      signIn: { token, id },
+                      signIn: { token, id, roles },
                     },
                   } = signInResult;
                   // save to localstorage
                   if (typeof window !== 'undefined') {
                     window.localStorage.setItem('token', token); // eslint-disable-line
                     window.localStorage.setItem('id', id); // eslint-disable-line
+                    window.localStorage.setItem('roles', roles); // eslint-disable-line
                     navigate('./onboarding');
                   }
-                });
+                })
+                .catch(error => {
+                  const errorMessage = _.get(error, 'message', '').split(
+                    'GraphQL error: ',
+                  );
+                  if (errorMessage.length > 1) {
+                    setError(errorMessage[1]);
+                  }
+                })
+                .finally(() => setLoading(false));
             }
           }}
         />
@@ -75,6 +97,7 @@ const signIn = gql`
     signIn(roomnumber: $roomnumber, password: $password) {
       id
       token
+      roles
     }
   }
 `;
--- src/pages/onboarding.js
+++ src/pages/onboarding.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import _ from 'lodash';
 import { navigate } from 'gatsby';
 import gql from 'graphql-tag';
 import { compose, graphql } from 'react-apollo';
@@ -9,26 +10,41 @@ import Margin from '../components/cowo/mobile/margin/Margin';
 
 const Onboarding = ({ updateUsername }) => {
   const [username, setUsername] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
   return (
     <Margin>
       <h2>Welcome!</h2>
+      <p>{error}</p>
       <div>
-        <span>What's your name?</span>
+        <span>What`s your name?</span>
         <TextField
           type="text"
           label="Name"
           placeholder=""
           onChange={value => setUsername(value)}
+          maxLength="20"
         />
+        <Margin />
         <ButtonContainer type="loose">
           <Button
             text="Let's Go!"
+            loading={loading}
             onPress={() => {
-              if (username !== '') {
+              if (username.trim() !== '') {
                 if (typeof window !== 'undefined') {
-                  updateUsername({ variables: { username } }).then(() =>
-                    navigate('./request'),
-                  );
+                  updateUsername({ variables: { username } })
+                    .then(() => navigate('./request'))
+                    .catch(error_ => {
+                      const errorMessage = _.get(error_, 'message', '').split(
+                        'GraphQL error: ',
+                      );
+                      if (errorMessage.length > 1) {
+                        setError(errorMessage[1]);
+                      }
+                    })
+                    .finally(() => setLoading(false));
                 }
               }
             }}
