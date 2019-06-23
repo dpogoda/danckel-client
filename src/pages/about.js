ebeed89b90f735e674802fa7d9c2c9684eac8791
David Pogorzelski <d.pogorzelski91@gmail.com>
2019-06-23 11:56:50 +0200

    Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Margin from '../components/cowo/mobile/margin/Margin';
+
+const About = () => (
+  <Margin>
+    <h3>About</h3>
+    <p>DanckelApp is a really cool app.</p>
+  </Margin>
+);
+
+export default About;
