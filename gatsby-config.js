08760ae7878e2ae59b72edcc76106ae36c1223c3
David Pogorzelski <d.pogorzelski91@gmail.com>
2019-06-23 11:55:17 +0200

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `DanckelApp`,
+    description: ``,
+    author: `Dorothea Wiemann & David Pogorzelski`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
