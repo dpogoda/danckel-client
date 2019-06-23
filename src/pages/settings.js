067c7a67efda3ee9be02a8f0771a938d766b97cc
David Pogorzelski <d.pogorzelski91@gmail.com>
2019-06-23 11:55:51 +0200

    Add settings page

---
+++ src/pages/settings.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { navigate } from 'gatsby';
+import Margin from '../components/cowo/mobile/margin/Margin';
+import Button from '../components/cowo/mobile/button/Button';
+import packageJson from '../../package.json';
+
+const Settings = () => (
+  <Margin>
+    <Button text="About" onPress={() => navigate('/about')} />
+    <Margin />
+    <Button text="Send Feedback" />
+    <Margin />
+    <Button text="Change password" />
+    <Margin />
+    <Button
+      text="Logout"
+      onPress={() => {
+        /* eslint-disable */
+        if (typeof window !== 'undefined') {
+          localStorage.removeItem('token');
+          localStorage.removeItem('id');
+          localStorage.removeItem('roles');
+          navigate('/');
+        }
+        /* eslint-enable */
+      }}
+    />
+    <Margin />
+    <p style={{ textAlign: 'center' }}>
+      Version:
+      {packageJson.version}
+    </p>
+  </Margin>
+);
+
+export default Settings;
